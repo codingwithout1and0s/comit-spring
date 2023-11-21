31d617ffb3cd12639446888cd7b2d870f62f4e57
NT Nguyen <codingwithout1and0s@gmail.com>
2023-11-20 19:38:33 -0600

    Modified some files

---
+++ src/main/java/com/comit/spring/controller/_02_Welcome.java
@@ -0,0 +1,20 @@
+package com.comit.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class _02_Welcome {
+
+	@GetMapping("/welcome")
+	@ResponseBody String welcome() {
+		return "<htm>l"
+				+ "<body>"
+				+ "<h1>Hello World</h1>"
+				+ "<a href='http://www.google.com>Google</a>"
+				+ "</body>"
+				+ "</html>";
+	}
+	
+}
