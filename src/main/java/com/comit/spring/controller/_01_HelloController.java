c19a58b75164c29afeae3cdac7214575d51dfb84
NT Nguyen <codingwithout1and0s@gmail.com>
2023-11-20 18:15:31 -0600

    Added HelloController

--- src/main/java/com/comit/spring/controller/_01_HelloController.java
+++ src/main/java/com/comit/spring/controller/_01_HelloController.java
@@ -1,10 +1,15 @@
 package com.comit.spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class _01_HelloController {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@GetMapping("/hello")
+	@ResponseBody
+	String hello() {
+		return "Hello, I love ComIT";
 	}
-
 }
