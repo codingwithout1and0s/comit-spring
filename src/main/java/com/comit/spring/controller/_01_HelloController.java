d054b7a0b4c020256c6cfd1421983049a790fadb
NT Nguyen <codingwithout1and0s@gmail.com>
2023-11-20 18:11:08 -0600

    First commit

---
+++ src/main/java/com/comit/spring/controller/_01_HelloController.java
@@ -0,0 +1,10 @@
+package com.comit.spring.controller;
+
+public class _01_HelloController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/comit/spring/ComitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comit.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
